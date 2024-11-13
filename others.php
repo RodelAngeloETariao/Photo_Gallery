be2b201d20d6409d2767989df5d7c3fc09cd078b
RodelAngeloETariao <153353367+RodelAngeloETariao@users.noreply.github.com>
2024-11-12 17:59:36 -0800

    Add files via upload

---
+++ others.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Photo Gallery</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/lightbox.css">
+<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/lightbox.js"></script>
+        <style>
+            .thumbnail img{
+                width: 350px;
+                height: 200px;
+
+            }
+            </style>
+
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <div class="navbar-brand">One Piece</div>
+</div>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="straw_hat.php">Straw Hat Pirates</a></li>
+            <li><a href="others.php">Others</a></li>
+
+</ul>
+</div>
+</nav>
+            <div class="container">
+                
+        <div class="page-header">
+            <h3>Yonko</h3>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <a href="pic/gear5.jpg" data-lightbox="gallery" data-title="Luffy" class="thumbnail">
+            <img src="pic/gear5.jpg">
+</a>
+           
+        </div>
+<div class="col-md-4">
+        <a href="pic/shanks.jpg" data-lightbox="gallery" data-title="Shanks" class="thumbnail">
+            <img src="pic/shanks.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/buggy.jpg" data-lightbox="gallery" data-title="Buggy"  class="thumbnail">
+            <img src="pic/buggy.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/blackbeard.jpg" data-lightbox="gallery" data-title="BlackBeard"  class="thumbnail">
+            <img src="pic/blackbeard.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/kaido.jpg" data-lightbox="gallery" data-title="Kaido"  class="thumbnail">
+            <img src="pic/kaido.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/bigmom.jpg" data-lightbox="gallery" data-title="BigMom"  class="thumbnail">
+            <img src="pic/bigmom.jpg">
+</a>
+</div>
+</div>
+    <footer class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container">
+            <p class="text-center" style="padding: 10px;">Created by: Eiichiro Oda</p>
+        </div>
+        </footer>
+
+
+</div>
+
+
+</div>
+</body>
+</html>
---
+++ straw_hat.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Photo Gallery</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/lightbox.css">
+<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/lightbox.js"></script>
+        <style>
+            .thumbnail img{
+                width: 350px;
+                height: 200px;
+
+            }
+            </style>
+
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <div class="navbar-brand">One Piece</div>
+</div>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="straw_hat.php">Straw Hat Pirates</a></li>
+            <li><a href="others.php">Others</a></li>
+
+</ul>
+</div>
+</nav>
+            <div class="container">
+                
+        <div class="page-header">
+            <h3>Straw Hat Pirates</h3>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <a href="pic/gear5.jpg" data-lightbox="gallery" data-title="Luffy" class="thumbnail">
+            <img src="pic/gear5.jpg">
+</a>
+           
+        </div>
+<div class="col-md-4">
+        <a href="pic/zoro.jpg" data-lightbox="gallery" data-title="Zoro" class="thumbnail">
+            <img src="pic/zoro.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/sanji.jpg" data-lightbox="gallery" data-title="Sanji"  class="thumbnail">
+            <img src="pic/sanji.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/jinbe.jpg" data-lightbox="gallery" data-title="Jinbe"  class="thumbnail">
+            <img src="pic/jinbe.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/usopp.jpg" data-lightbox="gallery" data-title="Usopp"  class="thumbnail">
+            <img src="pic/usopp.jpg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/nami.png" data-lightbox="gallery" data-title="Nami"  class="thumbnail">
+            <img src="pic/nami.png">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/chopper.jpeg" data-lightbox="gallery" data-title="Chopper"  class="thumbnail">
+            <img src="pic/chopper.jpeg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/robin.jpeg" data-lightbox="gallery" data-title="Robin"  class="thumbnail">
+            <img src="pic/robin.jpeg">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/franky.png" data-lightbox="gallery" data-title="Franky"  class="thumbnail">
+            <img src="pic/franky.png">
+</a>
+</div>
+<div class="col-md-4">
+        <a href="pic/brook.jpg" data-lightbox="gallery" data-title="Brook"  class="thumbnail">
+            <img src="pic/brook.jpg">
+</a>
+</div>
+    <footer class="navbar navbar-default navbar-fixed-bottom">
+        <div class="container">
+            <p class="text-center" style="padding: 10px;">Created by: Eiichiro Oda</p>
+        </div>
+        </footer>
+
+
+</div>
+
+
+</div>
+</body>
+</html>
